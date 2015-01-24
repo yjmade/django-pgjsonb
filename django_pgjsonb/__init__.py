023d6d7b58a26f0bf19aa3a88856a76dee2111d8
yj@yjmade.net <yj@yjmade.net>
2015-01-25 02:50:53 +0800

    fix setup config

--- django_pgjsonb/__init__.py
+++ django_pgjsonb/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from .fields import *  # noqa
 
-VERSION = __version__ = (0, 0, 1)
+VERSION = __version__ = (0, 0, 3)
--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
 from setuptools import setup, find_packages
-README = open('README.md').read()
 
 setup(
     name='django-pgjsonb',
-    version="0.0.1",
+    version="0.0.3",
     description='Django Postgres JSONB Fields support with lookups',
-    long_description=README,
     author='Jay Young(yjmade)',
     author_email='dev@yjmade.net',
     packages=find_packages(),
-    install_requires=['Django','psycopg2>=2.5.4'],
+    install_requires=['Django>=1.7','psycopg2>=2.5.4'],
 )
