7abbdb4df5bd3f0f01a937a27f657755eae69723
yj@yjmade.net <yj@yjmade.net>
2015-01-25 04:06:54 +0800

    [FIX]static files include and read

--- django_pgjsonb/__init__.py
+++ django_pgjsonb/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
 from .fields import *  # noqa
+import pkg_resources
 
-VERSION = __version__ = (0, 0, 3)
+VERSION = __version__ = pkg_resources.resource_string('django_pgjsonb', 'VERSION')
--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
+
+README = open('README.md').read()
+VERSION = open("django_pgjsonb/VERSION").read()
 
 setup(
     name='django-pgjsonb',
-    version="0.0.3",
+    version=VERSION,
     description='Django Postgres JSONB Fields support with lookups',
+    long_description=README,
     author='Jay Young(yjmade)',
     author_email='dev@yjmade.net',
     packages=find_packages(),
     install_requires=['Django>=1.7','psycopg2>=2.5.4'],
+    data_files=[('django_pgjsonb', ['django_pgjsonb/VERSION'])],
 )
