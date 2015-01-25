50c7397073db57bce8206bee422be1ac946b9eae
yj@yjmade.net <yj@yjmade.net>
2015-01-25 23:01:37 +0800

    add github url in setup file

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     name='django-pgjsonb',
     version=VERSION,
     description='Django Postgres JSONB Fields support with lookups',
+    url="https://github.com/yjmade/django-pgjsonb",
     long_description=README,
     author='Jay Young(yjmade)',
     author_email='dev@yjmade.net',
